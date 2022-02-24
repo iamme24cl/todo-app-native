bd0175a52b1c0bd7f40b6e33bad71aab7e94b300
chandra <iamme24cl@gmail.com>
2022-02-24 13:20:58 -0800

    implemented Todo item component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TodoItem from './components/TodoItem'
+import { getTodos, createTodo, updateTodoStatus, removeTodo} from './utils/api';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>My Todo List</Text>
+      <TodoItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +19,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: {
+    fontSize: 28,
+    marginTop: 70
+  }
 });
---
+++ components/TodoItem.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const TodoItem = (props) => {
+  const { todo, handleUpdateTodoStatus, handleTodoRemove } = props;
+
+  const show = false;
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => alert('clicked')}>
+        {show ? (
+          <View style={[styles.statusContainer, styles.todoComplete]}>
+            <Text>✓</Text>
+          </View>) : (
+            <View style={[styles.statusContainer, styles.todoInComplete]}></View>
+          )}
+      </TouchableOpacity>
+      <View>
+        <Text style={styles.taskContainer}>random todo</Text>
+      </View>
+      <View style={styles.closeBtnContainer}>
+        <TouchableOpacity onPress={() => alert('clicked')}>
+          <MaterialIcons name="delete" size={18} color='black' />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 15,
+    borderWidth: 1,
+    borderRadius: 6,
+    borderColor: '#ececec',
+    paddingHorizontal: 16,
+    paddingVertical: 6,
+    width: 320
+  },
+  statusContainer: {
+    alignItems: 'center',
+    justifyContent:'center',
+    width: 30,
+    height: 30,
+    marginRight: 15,
+    color: '#fff',
+  },
+  todoComplete: {
+    backgroundColor: '#1abc9c',
+    borderRadius: 50,
+  },
+  todoInComplete: {
+    borderRadius: 50,
+    borderColor: '#ececec',
+    borderWidth: 1
+  },
+  taskContainer: {
+    fontSize: 18,
+    width: 220
+  },
+  closeBtnContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+
+
+export default TodoItem;
